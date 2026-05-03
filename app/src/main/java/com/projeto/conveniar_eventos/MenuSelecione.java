dcd1abfaa1619a61db11690dd0592b62e124c431
lilicesm <alicesm3012@gmail.com>
2026-05-02 21:28:15 -0300

    fix: tentando personalizar o texto do dropdown

--- app/src/main/java/com/projeto/conveniar_eventos/MenuSelecione.java
+++ app/src/main/java/com/projeto/conveniar_eventos/MenuSelecione.java
@@ -1,6 +1,8 @@
 package com.projeto.conveniar_eventos;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,23 @@ public class MenuSelecione extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Spinner spinnerFundacoes = findViewById(R.id.spinner_fundacoes);
+        // como ainda não temos conexão com a API deixei só exemplos
+        String[] listaExemplo = {
+                "Selecione",
+                "FEPE — Fundação de Apoio ao Ensino, Pesquisa e Extensão",
+                "FUNTEF-PR — Fundação de Apoio à UTFPR",
+                "FACEV — Fundação de apoio da UFV"
+        };
+
+        // usando o Adapter pra conseguir modificar o padrao de texto do spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                R.layout.item_spinner,
+                listaExemplo
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerFundacoes.setAdapter(adapter);
     }
 }
