aac5440296c559c5f2ac00daf1b60a6194d1b344
lilicesm <alicesm3012@gmail.com>
2026-05-02 21:34:35 -0300

    feat: navegação pra tela do menu

--- app/src/main/java/com/projeto/conveniar_eventos/ui/MainActivity.java
+++ app/src/main/java/com/projeto/conveniar_eventos/ui/MainActivity.java
@@ -1,6 +1,8 @@
 package com.projeto.conveniar_eventos.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +10,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.projeto.conveniar_eventos.MenuSelecione;
 import com.projeto.conveniar_eventos.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,5 +25,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void navega_tela_menu(View v){
+        Intent it = new Intent(this, MenuSelecione.class);
+        startActivity(it);
     }
 }
